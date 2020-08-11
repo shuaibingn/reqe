c9e9ed8eaf126a20dd0402c272bd1cf620f1cbe4
niushuaibing <niushuaibing@foxmail.com>
2020-08-11 18:19:46 +0800

    update README

--- example/example.py
+++ example/example.py
@@ -11,7 +11,7 @@ if response:
 # delay: time between two requests
 # backoff: delay = delay * backoff
 response = reqe.get("https://www.google.com", retries=3, delay=3, backoff=2, timeout=(2, 2))
-# when `requests` raise an exception, `reqe` will catch the exception and response is None
+# when `requests` raise an exception, `reqe` will catch it and response is None
 if response:
     print(response)
 
